6a4a35675db4c3c413f86711aea30bc62f78723a
Асем Ержанова <sultik.99.10@gmail.com>
2019-02-13 02:23:50 +0600

    Task4

---
+++ Week 1/Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //read single number n and convert it
+
+            string[,] arr = new string[n, n]; // new array of strings to n*n
+        
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                for (int j = 0; j < i + 1; j++) arr[i, j] = "[*]";// put our symbol to 2D array
+                
+            }
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                for (int j = 0; j < n; j++) Console.Write(arr[i, j]);
+
+                Console.WriteLine(); // output our 2D array
+
+            } 
+        }
+    }
+}
