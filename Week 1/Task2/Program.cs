8b24555c7d3bc61424647f5cfd8a74b6f5c79d3e
Асем Ержанова <sultik.99.10@gmail.com>
2019-02-13 03:17:40 +0600

    Task2

---
+++ Week 1/Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+
+    class Student
+
+    {
+
+        public string name;
+
+        public string id;
+
+        public string year;
+
+
+
+        public Student()
+
+        {
+
+            name = Console.ReadLine();
+
+            id = Console.ReadLine();
+
+            year = Console.ReadLine();
+
+        }
+
+
+
+        public void PrintInfo()
+
+        {
+
+            int Year = int.Parse(year);
+
+            Console.WriteLine($"Name: {name} ID: {id} Year: {Year}");
+
+            Year++;
+
+            Console.WriteLine($"Name: {name} ID: {id} Year: {Year}");
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Student s = new Student();
+
+            s.PrintInfo();
+
+        }
+
+    }
+
+}
