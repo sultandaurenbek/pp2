a8445dbfed9da0eca89d4bfd9ceeca5c7dad7ce4
Асем Ержанова <sultik.99.10@gmail.com>
2019-02-14 02:10:02 +0600

    commit task2

--- Week 2/Task1/Program.cs
+++ Week 2/Task1/Program.cs
@@ -9,11 +9,11 @@ namespace Task1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             //to read text from files
-            FileStream fs = new FileStream(@"C:\input.txt", FileMode.Open, FileAccess.Read); // указываем путь к файлу
-            StreamReader s = new StreamReader(fs);
+            StreamReader s = new StreamReader(@"C:\input.txt");
             string l = s.ReadLine(); // saving read files to string l;
             bool k = true;
             for (int i = 0; i < l.Length; i++) // checking for polindrome
@@ -26,7 +26,7 @@ namespace Task1
             }
             if (k == true) Console.WriteLine("YES");
             else Console.WriteLine("NO");
-            fs.Close();
+            
             s.Close();
         }
     }
