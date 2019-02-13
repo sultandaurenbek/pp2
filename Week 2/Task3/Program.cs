033086cd2ea61c8e8a2172f62ff016a32fb4ea34
Асем Ержанова <sultik.99.10@gmail.com>
2019-02-13 19:23:22 +0600

    task3

---
+++ Week 2/Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static string s = "   ";
+        static void F(DirectoryInfo dir, int k)//recursive function that prints info about directory
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;//folders are yellow
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write(s);
+            }
+            Console.Write(dir.Name);//function prints the name of directory first
+            Console.WriteLine();
+            var x = dir.GetFileSystemInfos();
+            foreach (var t in x)
+            {
+                if (t.GetType() == typeof(DirectoryInfo))//if its directory we call function with more spaces
+                {
+                    DirectoryInfo y = t as DirectoryInfo;
+                    F(y, k + 1);
+                }
+                if (t.GetType() == typeof(FileInfo))//if its file its just print the name of file
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    for (int i = 0; i < k + 1; i++)
+                    {
+                        Console.Write(s);
+                    }
+                    Console.Write(t);
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Users\Sultan\Desktop\Intel");
+            F(dirinfo, 0);//calling function
+        }
+    }
+}
